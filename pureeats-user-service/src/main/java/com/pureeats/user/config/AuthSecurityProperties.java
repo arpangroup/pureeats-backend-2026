47051227b0ddeff5b5c9b1838c10bcde219d334d
arpan jana <arpangroup1@gmail.com>
2026-08-30 02:20:50 +0530

    fix authentication

--- pureeats-user-service/src/main/java/com/pureeats/user/config/AuthSecurityProperties.java
+++ pureeats-user-service/src/main/java/com/pureeats/user/config/AuthSecurityProperties.java
@@ -98,7 +98,8 @@ public class AuthSecurityProperties {
     }
 
     public static class Session {
-        private int accessTokenExpiryMinutes = 15;
+        //private int accessTokenExpiryMinutes = 15;
+        private int accessTokenExpiryMinutes = 1440*3; // 3 day (1 day = 24*60 minute)
         private int refreshTokenExpiryDays = 30;
 
         public int getAccessTokenExpiryMinutes() {
